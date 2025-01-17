004a589dddd5aed565b83d640edad01a5c83fc23
AHMED6011 <ahmedallawi010@gmail.com>
2025-01-17 16:05:08 +0100

    ; added

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const titleH1 = document.querySelector("h1");
 titleH1.style.color = "red";
 titleH1.style.fontSize = "50px";
 console.log(`this text that we have got from html file ${titleH1}`);
-titleH1.style.fontWeight = "700"
+titleH1.style.fontWeight = "700";
